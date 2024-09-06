299d7e4447534807258ca520fa19c9b9a6aea7de
Takiuddin Ahmed <takiuddinahmed@gmail.com>
2024-09-06 17:33:31 +0600

    clean code

--- src/student/student.controller.ts
+++ src/student/student.controller.ts
@@ -1,6 +1,5 @@
 import { Request, Response } from "express";
 import { studentService } from "./student.service";
-import pool from "../server";
 const createStudent = async (req: Request, res: Response) => {
   try {
     const {
